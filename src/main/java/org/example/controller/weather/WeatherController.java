03b49ba93c34cab1cb1e371c5ac27c443f7dfd18
Park Hyewon <db1648@naver.com>
2024-08-22 17:42:32 +0900

    240822 forecastController 기능 weatherController 병합

--- src/main/java/org/example/controller/weather/WeatherController.java
+++ src/main/java/org/example/controller/weather/WeatherController.java
@@ -1,6 +1,7 @@
 package org.example.controller.weather;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.dto.forecast.ForecastDto;
 import org.example.dto.weather.WeatherDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -21,6 +22,8 @@ public class WeatherController {
 
     @Value("${weather.url}")
     private String URL;
+    @Value("${forecast.url}")
+    private String FORECAST_URL;
     @Value("${weather.api_key}")
     private String API_KEY;
 
@@ -39,5 +42,20 @@ public class WeatherController {
         log.info("오늘의 날씨: " + weather);
 
         return ResponseEntity.ok(weather);
+    }
+    @GetMapping("/forecast/{city}")
+    public ResponseEntity<ForecastDto> forest(@PathVariable("city") String city) {
+        city = city == null ? "seoul" : city;
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = UriComponentsBuilder.fromHttpUrl(FORECAST_URL)
+                .queryParam("q", city)
+                .queryParam("units", "metric")
+                .queryParam("APPID", API_KEY)
+                .queryParam("lang", "kr")
+                .toUriString();
+
+        ForecastDto forestDto = restTemplate.getForObject(url, ForecastDto.class);
+        return ResponseEntity.ok(forestDto);
     }
 }
