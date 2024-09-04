59395a2738fa082825828fd6f05a99110672dc4c
Park Hyewon <db1648@naver.com>
2024-09-04 11:21:07 +0900

    240904 boardmapper , boardservice 객체 구현

--- src/main/java/org/example/service/board/BoardService.java
+++ src/main/java/org/example/service/board/BoardService.java
@@ -1,4 +1,14 @@
 package org.example.service.board;
 
-public class BoardService {
+import org.springframework.stereotype.Service;
+import org.example.dto.board.BoardDto;
+import java.util.List;
+
+@Service
+public interface BoardService {
+    List<BoardDto> getList();
+    BoardDto detail(Long id);
+    void create(BoardDto board);
+    BoardDto update(BoardDto board);
+    BoardDto delete(Long id);
 }
